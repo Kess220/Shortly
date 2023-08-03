a3d07c4b282bc00e9148a473ab88af8acea68fc9
Kess220 <prodvictorkaio@gmail.com>
2023-08-03 18:25:08 -0300

    debug

--- src/controllers/signinController.js
+++ src/controllers/signinController.js
@@ -8,7 +8,8 @@ const generateAuthToken = (userId) => {
 };
 
 export const signin = async (req, res) => {
-  // Definir o esquema de validação do body
+  console.log("Entrando na função signin");
+
   const schema = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
@@ -17,6 +18,7 @@ export const signin = async (req, res) => {
   const { error } = schema.validate(req.body);
 
   if (error) {
+    console.log("Dados inválidos no body:", error);
     return res.status(422).json({ error: "Dados inválidos no body." });
   }
 
@@ -24,22 +26,27 @@ export const signin = async (req, res) => {
 
   try {
     // Verificar se o usuário existe com o e-mail fornecido
+    console.log("Consultando usuário no banco de dados...");
     const existingUser = await db.query(
       "SELECT * FROM users WHERE email = $1",
       [email]
     );
     if (existingUser.rows.length === 0) {
+      console.log("Usuário não encontrado.");
       return res.status(401).json({ error: "Credenciais inválidas." });
     }
 
     // Verificar se a senha é válida
+    console.log("Comparando senhas...");
     const user = existingUser.rows[0];
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
+      console.log("Senha inválida.");
       return res.status(401).json({ error: "Credenciais inválidas." });
     }
 
     // Gerar o token de autenticação
+    console.log("Gerando token...");
     const token = generateAuthToken(user.id);
 
     res.cookie("authToken", token, {
@@ -49,6 +56,7 @@ export const signin = async (req, res) => {
     });
 
     // Responder com o token no corpo da resposta
+    console.log("Autenticação bem-sucedida.");
     res.status(200).json({ token });
   } catch (error) {
     console.error("Erro ao autenticar usuário:", error);
